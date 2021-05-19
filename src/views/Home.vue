e8ba9f2a855fdbbd0ae27ef0ab256c05e8e2cd28
Terrynightbleach <terrynightbleach@gmail.com>
2021-05-19 18:22:52 +0800

    project Initialization

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button type="primary">Carousel-project with ElementUI<i class="el-icon-sunset el-icon--right"></i></el-button>
+    <el-button type="primary">Carousel-project(hand-writing)<i class="el-icon-moon-night el-icon--right"></i></el-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style scoped>
+  .btn {
+    margin: 60px auto;
+  }
+</style>
